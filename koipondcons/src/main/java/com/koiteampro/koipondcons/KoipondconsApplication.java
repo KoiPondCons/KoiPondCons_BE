26bbecc35292009e19e932397928e9b3501eefc9
Mai Hau <haum747@gmail.com>
2024-09-23 15:33:40 +0700

    Initialize project

---
+++ koipondcons/src/main/java/com/koiteampro/koipondcons/KoipondconsApplication.java
@@ -0,0 +1,13 @@
+package com.koiteampro.koipondcons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoipondconsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoipondconsApplication.class, args);
+	}
+
+}
