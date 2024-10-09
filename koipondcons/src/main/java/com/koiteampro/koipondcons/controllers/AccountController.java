73fa498a9af6f50c6fec25d1acface09f8cbe642
Tran Kim Nha <nhatkse182290@fpt.edu.vn>
2024-10-10 03:08:56 +0700

    add api get free staff

---
+++ koipondcons/src/main/java/com/koiteampro/koipondcons/controllers/AccountController.java
@@ -0,0 +1,36 @@
+package com.koiteampro.koipondcons.controllers;
+
+import com.koiteampro.koipondcons.entities.Account;
+import com.koiteampro.koipondcons.services.DesignDrawingService;
+import com.koiteampro.koipondcons.services.StaffConstructionDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/account")
+@CrossOrigin("*")
+public class AccountController {
+    @Autowired
+    StaffConstructionDetailService staffConstructionDetailService;
+
+    @Autowired
+    DesignDrawingService designDrawingService;
+
+    @GetMapping("/free-constructors")
+    public ResponseEntity getAllFreeConstructors() {
+        List<Account> accounts = staffConstructionDetailService.getAllFreeConstructors();
+        return ResponseEntity.ok(accounts);
+    }
+
+    @GetMapping("/free-designers")
+    public ResponseEntity getAllFreeDesigners() {
+        List<Account> accounts = designDrawingService.getAllFreeDesigners();
+        return ResponseEntity.ok(accounts);
+    }
+}
