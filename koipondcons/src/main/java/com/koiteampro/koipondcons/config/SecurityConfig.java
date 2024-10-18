8710bbd21da33c3619d03a2cb48ca629b66a9c44
Mai Hau <haum747@gmail.com>
2024-10-18 17:36:01 +0700

    Modify vnpay controller

--- koipondcons/src/main/java/com/koiteampro/koipondcons/config/SecurityConfig.java
+++ koipondcons/src/main/java/com/koiteampro/koipondcons/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
                                 .requestMatchers("/api/orders/**", "/api/design-drawings/**", "/api/comboprices/**").permitAll()
                                 .requestMatchers("/api/account/**").permitAll()
                                 .requestMatchers("/api/demo").permitAll()
-                                .requestMatchers("/submitOrder", "/vnpay-payment-return").permitAll()
+                                .requestMatchers("api/submitOrder", "api/vnpay-payment-return").permitAll()
                                 .anyRequest()
                                 .authenticated()
 
