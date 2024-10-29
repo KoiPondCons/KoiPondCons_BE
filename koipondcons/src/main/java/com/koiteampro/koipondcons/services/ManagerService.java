daabd13d771d77198c1d9db8e0866b8632a1de49
Tran Kim Nha <nhatkse182290@fpt.edu.vn>
2024-10-29 15:49:33 +0700

    add count total order by combo

--- koipondcons/src/main/java/com/koiteampro/koipondcons/services/ManagerService.java
+++ koipondcons/src/main/java/com/koiteampro/koipondcons/services/ManagerService.java
@@ -4,12 +4,15 @@ import com.koiteampro.koipondcons.enums.ConstructionOrderStatus;
 import com.koiteampro.koipondcons.enums.MaintenanceOrderStatus;
 import com.koiteampro.koipondcons.enums.Role;
 import com.koiteampro.koipondcons.repositories.AccountRepository;
+import com.koiteampro.koipondcons.repositories.ComboRepository;
 import com.koiteampro.koipondcons.repositories.ConstructionOrderRepository;
 import com.koiteampro.koipondcons.repositories.MaintenanceOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -23,6 +26,9 @@ public class ManagerService {
 
     @Autowired
     MaintenanceOrderRepository maintenanceOrderRepository;
+
+    @Autowired
+    ComboRepository comboRepository;
 
     public Map<String, Object> getDashboardStats() {
         Map<String, Object> stats = new HashMap<>();
@@ -38,6 +44,17 @@ public class ManagerService {
 
         long totalMaintenanceOrders = maintenanceOrderRepository.countByStatus(MaintenanceOrderStatus.FINISHED);
         stats.put("totalMaintenanceOrders", totalMaintenanceOrders);
+
+        List<Object[]> totalOrdersByCombo = comboRepository.countOrdersOfCombo();
+        List<Map<String, Object>> totalOrdersByComboList = new ArrayList<>();
+
+        for (Object[] row : totalOrdersByCombo) {
+            Map<String, Object> comboInfo = new HashMap<>();
+            comboInfo.put("comboName", row[0]);
+            comboInfo.put("totalOrders", row[1]);
+            totalOrdersByComboList.add(comboInfo);
+        }
+        stats.put("totalOrdersByCombo", totalOrdersByComboList);
 
         return stats;
     }
