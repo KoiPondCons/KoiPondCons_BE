1aee254890c9e06d391cbaed013083d740524765
Tran Kim Nha <nhatkse182290@fpt.edu.vn>
2024-10-29 15:49:21 +0700

    add count total order by combo

--- koipondcons/src/main/java/com/koiteampro/koipondcons/repositories/ComboRepository.java
+++ koipondcons/src/main/java/com/koiteampro/koipondcons/repositories/ComboRepository.java
@@ -2,10 +2,19 @@ package com.koiteampro.koipondcons.repositories;
 
 import com.koiteampro.koipondcons.entities.Combo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ComboRepository extends JpaRepository<Combo, Long> {
 
     List<Combo> findAllByIsDeletedFalse();
+
+    @Query(
+            "select cb.name, count(*) as totalOrders\n" +
+                    "from Quotation q join q.constructionOrder co join q.combo cb\n" +
+                    "where co.status = 'CLOSED'\n" +
+                    "group by q.combo.id"
+    )
+    List<Object[]> countOrdersOfCombo();
 }
