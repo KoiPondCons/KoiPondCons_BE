9f8e123aedadf669d2302fec5569e2a0d13ef52f
Mai Hau <haum747@gmail.com>
2024-11-13 08:02:07 +0700

    Set url for front-eand

--- koipondcons/src/main/java/com/koiteampro/koipondcons/config/SecurityConfig.java
+++ koipondcons/src/main/java/com/koiteampro/koipondcons/config/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.addAllowedOrigin("http://localhost:5173"); // Replace with your frontend URL
+        configuration.addAllowedOrigin("https://koipondcons.onrender.com"); // Replace with your frontend URL
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
--- koipondcons/src/main/java/com/koiteampro/koipondcons/controllers/VNPayController.java
+++ koipondcons/src/main/java/com/koiteampro/koipondcons/controllers/VNPayController.java
@@ -55,7 +55,7 @@ public class VNPayController {
 
         MaintenanceOrderResponse maintenanceOrderResponse = maintenanceOrderService.getByMaintenanceOrderId(Long.parseLong(paymentId));
 
-        String baseUrl = "http://localhost:5173";
+        String baseUrl = "https://koipondcons.onrender.com";
 
         return vnPayService.createOrder(request, (long)maintenanceOrderResponse.getPrice()*100, "maintain/" + String.valueOf(maintenanceOrderResponse.getId()), baseUrl);
     }
